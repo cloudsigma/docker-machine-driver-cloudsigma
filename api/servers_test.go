d0c02fb4cfc6337abf0687405cae56d2669b8db9
pavel <36902936+pavel-github@users.noreply.github.com>
2018-03-22 14:24:40 +0100

    #7: Complete test server methods

--- api/servers_test.go
+++ api/servers_test.go
@@ -9,6 +9,65 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestServers_AttachDrive(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	input := &AttachDriveRequest{
+		Drives: []ServerDrive{
+			{BootOrder: 1, DevChannel: "0:0", Device: "virtio", DriveUUID: "drive-uuid"},
+		},
+	}
+	mux.HandleFunc("/servers/long-uuid/", func(w http.ResponseWriter, r *http.Request) {
+		v := new(AttachDriveRequest)
+		json.NewDecoder(r.Body).Decode(v)
+		assert.Equal(t, input, v)
+
+		assert.Equal(t, "PUT", r.Method)
+		assert.Equal(t, authorizationHeader, r.Header.Get("Authorization"))
+		fmt.Fprint(w, `{"cpu":100,"mem":200}`)
+	})
+	expected := &Server{
+		CPU:    100,
+		Memory: 200,
+	}
+
+	server, _, err := client.Servers.AttachDrive("long-uuid", input)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, server)
+}
+
+func TestServers_AttachDrive_emptyPayload(t *testing.T) {
+	client, _, _, teardown := setup()
+	defer teardown()
+
+	_, _, err := client.Servers.AttachDrive("long-uuid", nil)
+
+	assert.Error(t, err)
+	assert.Equal(t, ErrEmptyPayloadNotAllowed.Error(), err.Error())
+}
+
+func TestServers_AttachDrive_emptyServerUUID(t *testing.T) {
+	client, _, _, teardown := setup()
+	defer teardown()
+	attachDriveRequest := &AttachDriveRequest{}
+
+	_, _, err := client.Servers.AttachDrive("", attachDriveRequest)
+
+	assert.Error(t, err)
+	assert.Equal(t, ErrEmptyArgument.Error(), err.Error())
+}
+
+func TestServers_AttachDrive_invalidUUID(t *testing.T) {
+	client, _, _, teardown := setup()
+	defer teardown()
+	attachDriveRequest := &AttachDriveRequest{}
+
+	_, _, err := client.Servers.AttachDrive("%s", attachDriveRequest)
+
+	assert.Error(t, err)
+}
+
 func TestServers_Create(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
@@ -77,6 +136,38 @@ func TestServers_Delete_invalidUUID(t *testing.T) {
 	assert.Error(t, err)
 }
 
+func TestServers_doAction(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+	mux.HandleFunc("/servers/long-uuid/", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method)
+		fmt.Fprint(w, `{"action":"start","result":"server is starting","uuid":"uuid"}`)
+	})
+
+	_, _, err := client.Servers.doAction("long-uuid", "start")
+
+	assert.NoError(t, err)
+}
+
+func TestServers_doAction_emptyAction(t *testing.T) {
+	client, _, _, teardown := setup()
+	defer teardown()
+
+	_, _, err := client.Servers.doAction("long-uuid", "")
+
+	assert.Error(t, err)
+	assert.Equal(t, ErrEmptyArgument.Error(), err.Error())
+}
+
+func TestServers_doAction_invalidUUID(t *testing.T) {
+	client, _, _, teardown := setup()
+	defer teardown()
+
+	_, _, err := client.Servers.doAction("%", "start")
+
+	assert.Error(t, err)
+}
+
 func TestServers_Get(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
@@ -116,4 +207,34 @@ func TestServers_Get_invalidUUID(t *testing.T) {
 	_, _, err := client.Servers.Get("%")
 
 	assert.Error(t, err)
+}
+
+func TestServers_Start_emptyUUID(t *testing.T) {
+	client, _, _, teardown := setup()
+	defer teardown()
+
+	_, _, err := client.Servers.Start("")
+
+	assert.Error(t, err)
+	assert.Equal(t, ErrEmptyArgument.Error(), err.Error())
+}
+
+func TestServers_Stop_emptyUUID(t *testing.T) {
+	client, _, _, teardown := setup()
+	defer teardown()
+
+	_, _, err := client.Servers.Stop("")
+
+	assert.Error(t, err)
+	assert.Equal(t, ErrEmptyArgument.Error(), err.Error())
+}
+
+func TestServers_Shutdown_emptyUUID(t *testing.T) {
+	client, _, _, teardown := setup()
+	defer teardown()
+
+	_, _, err := client.Servers.Shutdown("")
+
+	assert.Error(t, err)
+	assert.Equal(t, ErrEmptyArgument.Error(), err.Error())
 }
